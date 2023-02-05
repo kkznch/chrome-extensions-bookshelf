258fd75b0075e92b64ea13bd649859488c8bb024
kkznch <kkznch710md@gmail.com>
2023-02-05 13:43:19 +0900

    Remove unnecessary package

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -21,9 +20,4 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-    }),
-  ],
 };
