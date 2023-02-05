8293949a7ec672ce5f15df57a3d866cb2744caf8
kkznch <kkznch710md@gmail.com>
2023-02-05 12:09:24 +0900

    Set ChakraProvider

--- index.tsx
+++ index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './src/App';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
