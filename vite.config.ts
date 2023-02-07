27b955594c0788b1fe8dd98efa2e17b9bb1788c5
kkznch <kkznch710md@gmail.com>
2023-02-07 23:13:26 +0900

    manifestはvite.config.js内に記述するようにした

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { crx } from '@crxjs/vite-plugin';
+import { crx, defineManifest } from '@crxjs/vite-plugin';
 import { resolve } from 'path';
-import manifest from './manifest.json';
+
+const manifest = defineManifest({
+  name: 'MyBookshelf',
+  version: '1.0.0',
+  manifest_version: 3,
+  description: 'Sample Chrome Extension',
+  permissions: ['bookmarks', 'favicon'],
+  chrome_url_overrides: {
+    newtab: './index.html',
+  },
+});
 
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
