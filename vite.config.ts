1bd43fe3092ec12c3bc631aa9987f993f5d6bc0b
kkznch <kkznch710md@gmail.com>
2023-08-05 17:33:00 +0900

    add icons

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,10 @@ const manifest = defineManifest({
   manifest_version: 3,
   description: 'Sample Chrome Extension',
   permissions: ['bookmarks'],
-  chrome_url_overrides: {
-    newtab: 'index.html',
+  icons: {
+    '16': 'icon16.png',
+    '48': 'icon48.png',
+    '128': 'icon128.png',
   },
 });
 
